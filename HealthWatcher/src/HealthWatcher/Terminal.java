4a04d70d3bba19cbc19d5ef5b2dfbf9721763266
Gonza <gonzaloalcalde96@gmail.com>
2018-04-24 20:04:49 -0300

    Minor text fixes

--- HealthWatcher/src/HealthWatcher/Terminal.java
+++ HealthWatcher/src/HealthWatcher/Terminal.java
@@ -1,9 +1,35 @@
 package HealthWatcher;
 
 public class Terminal {
-    public void menu(){
-        //Citizen selects the kind of complaint
 
+    public void mainMenu(){
+        //Citizen selects the kind of complaint
+        int complaint = 0;
+        switch (complaint) {
+            case 1:
+                animalComplaintMenu();
+                break;
+            case 2:
+                foodComplaintMenu();
+                break;
+            case 3:
+                specialComplaintMenu();
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    public void animalComplaintMenu(){
+
+    }
+
+    public void foodComplaintMenu(){
+
+    }
+
+    public void specialComplaintMenu(){
 
     }
 }
